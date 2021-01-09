fc9f07078b52bcf95505bb9209c722f856ca7fc4
andrewh-code <andrewh.code@gmail.com>
2021-01-09 17:08:12 -0500

    added archive controller through rails g controller archive all

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # draw each route according to what resources are available
 Rails.application.routes.draw do
+  get 'archive/index'
   resources :entries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
