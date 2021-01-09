41ee0664eeadcb802e52a0701d2c07c48c0f4324
andrewh-code <andrewh.code@gmail.com>
2021-01-09 16:27:18 -0500

    initial commit for food log

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# draw each route according to what resources are available
+Rails.application.routes.draw do
+  resources :entries
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # going to go to the entires controller and directly to the index function in that controller
+  # (localhost:3000 front page)
+  root to: "entries#index"
+
+end
